79a1901ac8e12e5ff19659ee180002afed0d0965
Tanets Olexandr <104691488+AlexTanetsDev@users.noreply.github.com>
2023-11-17 19:35:48 +0200

    delete commented lines

--- src/helpers/aws.s3.ts
+++ src/helpers/aws.s3.ts
@@ -55,10 +55,4 @@ export class PhotoS3Loader {
   }
 }
 
-// export async function downloadImage(key: string) {
-//   import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-//   import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
-//   const client = new S3Client(clientParams);
-//   const command = new GetObjectCommand(getObjectParams);
-//   const url = await getSignedUrl(client, command, { expiresIn: 3600 });
-// }
+
